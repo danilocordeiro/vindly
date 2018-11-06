4475771180a315ba33561e908686a628dfd47a6e
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2018-11-06 19:35:35 -0200

    Add some config to deploy

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const winston = require('winston');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 
@@ -8,6 +6,8 @@ require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
+require('./startup/validation')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
